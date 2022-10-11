acaf4c3fdca4970518ee86ce9dfa68d714cc73a9
Jutamas25244 <73680085+Jutamas25244@users.noreply.github.com>
2022-10-11 10:58:21 +0700

    Week9

---
+++ product-server/src/main/java/com/sop/week9/productserver/event/ProductCreatedEvent.java
@@ -0,0 +1,13 @@
+package com.sop.week9.productserver.event;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductCreatedEvent {
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
