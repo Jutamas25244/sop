3b0ad56b02302a9abc944e792b6e3cf207710c3f
Jutamas25244 <73680085+Jutamas25244@users.noreply.github.com>
2022-11-08 12:11:00 +0700

    week12

---
+++ Week 12/gateway-service/gateway-service/src/main/java/com/chapter12/gatewayservice/config/JwtUtil.java
@@ -0,0 +1,22 @@
+package com.chapter12.gatewayservice.config;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.security.Key;
+
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes())
+    }
+}
